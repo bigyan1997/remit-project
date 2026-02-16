5a2066771e13788e6fd6ef65d746a6289ab2711a
Bigyan Karki <karkibigyan05@gmail.com>
2026-02-16 17:06:16 +1100

    Fixed syntax error and completed user migrations

--- backend/settings.py
+++ backend/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'core',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'core.User'
